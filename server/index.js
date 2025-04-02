a9da0fa1a9bf9dff6f9622b30f503fc92213e029
Thilan Hasitha <156594121+thilanhasitha@users.noreply.github.com>
2025-04-02 15:00:15 +0530

    mongo database is connected to the project

--- server/index.js
+++ server/index.js
@@ -1,7 +1,21 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import cors from'cors'
+import dotenv from 'dotenv'
 
-
+dotenv.config();
 const app = express();
+
+//connect to the mongo database
+mongoose.connect(process.env.MONGO)
+.then(()=>{
+    console.log("Mongo database is running successfully")
+})
+.catch((error)=>{
+    console.log(error)
+})
+
+
 
 app.use(express.json());
 
