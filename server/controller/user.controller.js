5d4405e6c4f7bff1ecf29bf6813963d06054784d
Thilan Hasitha <156594121+thilanhasitha@users.noreply.github.com>
2025-04-03 17:29:13 +0530

    create sign in api route

--- server/controller/user.controller.js
+++ server/controller/user.controller.js
@@ -1,6 +1,7 @@
 import bcryptjs from 'bcryptjs';
 import User from '../model/user.model.js';
 import { errorHandler } from '../utils/error.js';
+import jwt from 'jsonwebtoken';
 
 export const getAllUsers = (req, res, next) => {
     try {
@@ -30,5 +31,36 @@ export const addUsers = async(req,res,next)=>{
         next(errorHandler(550,'error from the function'));
     }
 };
+
+export const signin = async(req,res,next)=>{
+    const {email,password} = req.body;
+    try{
+        // firstly check the email is valid or not after check the password 
+        const validUser = await User.findOne({email});
+        if(!validUser) return next(errorHandler(404,'User not found'));
+
+        //password check
+        const validPassword = bcryptjs.compareSync(password,validUser.password);
+        if(!validPassword) return next(errorHandler(404,'wrong credentials'));
+
+        //create the token
+        const token = jwt.sign({id:validUser._id},process.env.JWT_SECRET)
+        //that is another thing
+        const{password:pass,...rest} = validUser._doc;
+
+        //token should be save inside the cookie
+        res
+        .cookie('access_token',token, {httpOnly:true,})
+        .status(200)
+        .json(rest);
+
+
+
+    }
+    catch(error){
+
+    }
+
+}
 
 
--- server/route/user.route.js
+++ server/route/user.route.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { addUsers, getAllUsers } from "../controller/user.controller.js";
+import { addUsers, getAllUsers, signin } from "../controller/user.controller.js";
 
 const route = Router();
 
 route.get('/getusers',getAllUsers);
 route.post('/adduser',addUsers);
+route.post('/sign-in',signin)
 
 export default route;
